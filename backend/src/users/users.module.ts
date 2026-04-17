62c554e7a6785a4be905a0d3d2d4beb585c1ba7f
miyatarutyyy <study.miyata026@gmail.com>
2026-04-17 20:28:31 +0900

    feat: UserEntity を追加してUsersModule にTypeORM 連携を追加

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -5,10 +5,13 @@
  * UserController と UserService を読み込む
  */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService],
 })
